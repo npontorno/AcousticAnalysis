b5e0a41d8dd39bd878e35f6e5bae8550c2d15245
EverettDesern <47554924+EverettDesern@users.noreply.github.com>
2020-04-28 17:46:00 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,8 @@
 import librosa
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib import pyplot as plt
 import numpy as np
 import noisereduce as nr
 import scipy as sp
